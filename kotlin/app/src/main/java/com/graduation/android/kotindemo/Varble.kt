0c9a84587607d0961812d2c481cb145e46345075
linbin <1207087645@qq.com>
2019-12-20 22:07:21 +0800

    kt 变量申明

---
+++ kotlin/app/src/main/java/com/graduation/android/kotindemo/Varble.kt
@@ -0,0 +1,29 @@
+package com.graduation.android.kotindemo
+
+
+//不同类型的变量声明
+class Varble {
+    var m: Int = 20//int 类型
+    var price: Double = 20.4//double 类型
+    var flag: Boolean = true
+    var v1: Int = 100
+    var v2: Double = 20.4
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
