eb791a11dec6c62a6aa4743c8bfe27af1e23ade1
linbin <1207087645@qq.com>
2019-12-20 21:51:17 +0800

    kt 加法函数

--- kotlin/app/src/main/java/com/graduation/android/kotindemo/MainActivity.kt
+++ kotlin/app/src/main/java/com/graduation/android/kotindemo/MainActivity.kt
@@ -3,6 +3,7 @@ package com.graduation.android.kotindemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 
 /**
@@ -13,9 +14,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var btn=findViewById<Button>(R.id.btn_ok)
-        btn.setOnClickListener{//输出print
-            println("你好吗？")
+        var btn = findViewById<Button>(R.id.btn_ok)
+
+        var tv_content = findViewById<TextView>(R.id.tv_content)
+        btn.setOnClickListener {
+            //输出print
+            tv_content.text=""+add(5, 8)
         }
+    }
+
+    fun add(m: Int, n: Int): Int {
+        return m + n
+
     }
 }
